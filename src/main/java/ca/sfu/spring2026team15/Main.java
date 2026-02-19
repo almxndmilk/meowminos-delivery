b4ac54fa2850b777138f0f6d9a601f7f3406d767
qdn <qdn@sfu.ca>
2026-02-19 15:45:27 -0800

    Add in WASD movements to the character

--- src/main/java/ca/sfu/spring2026team15/Main.java
+++ src/main/java/ca/sfu/spring2026team15/Main.java
@@ -2,30 +2,39 @@ package ca.sfu.spring2026team15;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.math.Vector2;
 
 public class Main extends ApplicationAdapter {
     private Texture catBike;
     private SpriteBatch spriteBatch;
     private FitViewport viewport;
+    private Texture background;
+    Vector2 position;
+    float moveSpeed;
 
     @Override
     public void create() {
+
         System.out.println("=== GAME CREATE ===");
         try {
-            catBike = new Texture(Gdx.files.internal("catBike.png"));
+            catBike = new Texture(Gdx.files.internal("cataseprite1.png"));
             System.out.println("✓ Texture loaded: " + catBike.getWidth() + "x" + catBike.getHeight());
         } catch (Exception e) {
             System.err.println("✗ FAILED TO LOAD TEXTURE: " + e.getMessage());
             e.printStackTrace();
         }
 
+        background = new Texture(Gdx.files.internal("IMG_5555.png"));
         spriteBatch = new SpriteBatch();
-        viewport = new FitViewport(8, 5);
+        position = new Vector2(0,0);
+        moveSpeed = 10;
+        viewport = new FitViewport(800, 500);
         System.out.println("✓ SpriteBatch and Viewport created");
     }
 
@@ -36,12 +45,27 @@ public class Main extends ApplicationAdapter {
 
     @Override
     public void render() {
+
+        if (Gdx.input.isKeyPressed(Input.Keys.W))
+            position.y += moveSpeed * Gdx.graphics.getDeltaTime();
+        else if(Gdx.input.isKeyPressed(Input.Keys.S))
+            position.y -= moveSpeed * Gdx.graphics.getDeltaTime();
+        if (Gdx.input.isKeyPressed(Input.Keys.D))
+            position.x += moveSpeed * Gdx.graphics.getDeltaTime();
+        else if (Gdx.input.isKeyPressed(Input.Keys.A))
+            position.x -= moveSpeed * Gdx.graphics.getDeltaTime();
+
         ScreenUtils.clear(Color.BLACK);
         spriteBatch.begin();
 
-        if (catBike != null) {
-            spriteBatch.draw(catBike, 100, 100, 256, 256);
-        }
+        float worldWidth = viewport.getWorldWidth();
+        float worldHeight = viewport.getWorldHeight();
+
+        spriteBatch.draw(background, 0,0, worldWidth, worldHeight);
+        spriteBatch.draw(catBike, position.x, position.y, 100, 100);
+//        if (catBike != null) {
+//
+//        }
         spriteBatch.end();
     }
 
