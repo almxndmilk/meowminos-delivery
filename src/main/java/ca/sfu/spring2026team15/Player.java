6fd92724d659013832c0ffa5972067a933b19e8a
Krish Kaushik <krishkaushk@cs.sfu.ca>
2026-02-23 12:16:33 -0800

    full all direction animation for player

--- src/main/java/ca/sfu/spring2026team15/Player.java
+++ src/main/java/ca/sfu/spring2026team15/Player.java
@@ -7,72 +7,93 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 
 public class Player {
-    private static final float SIZE = 128f;
-    private static final float SPEED = 300f;
+    private static final float SIZE = 64f;
+    private static final float SPEED = 400f;
 
-    private final Texture left1;
-    private final Texture left2;
+    private final Texture left1, left2;
+    private final Texture up1, up2, up3;
+    private final Texture down1, down2, down3;
+
+
+
     private Texture currentFrame;
+    private static final float FRAME_DURATION = 0.12f;
     private float animTimer = 0f;
-    private float frameDuration = 0.15f;
-    private boolean showFrame1 = true;
+    private int currentFrameIndex = 0;
     private boolean isMoving = false;
 
+    private enum Direction { LEFT, RIGHT, UP, DOWN }
+    private Direction lastDirection = Direction.DOWN;
 
     private final Vector2 position;
 
     public Player(float startX, float startY) {
         left1 = new Texture(Gdx.files.internal("catOffBike/catOffBike1.png"));
         left2 = new Texture(Gdx.files.internal("catOffBike/catOffBike2.png"));
-        currentFrame = left1;
+        up1   = new Texture(Gdx.files.internal("catBack/catBack1.png"));
+        up2   = new Texture(Gdx.files.internal("catBack/catBack2.png"));
+        up3   = new Texture(Gdx.files.internal("catBack/catBack3.png"));
+        down1 = new Texture(Gdx.files.internal("catFront/catFront1.png"));
+        down2 = new Texture(Gdx.files.internal("catFront/catFront2.png"));
+        down3 = new Texture(Gdx.files.internal("catFront/catFront3.png"));
+        currentFrame = down1;
         position = new Vector2(startX, startY);
     }
 
     public void update(float delta) {
         isMoving = false;
 
-        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
-            position.y += SPEED * delta;
-            isMoving = true;
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.S)) {
-            position.y -= SPEED * delta;
-            isMoving = true;
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
-            position.x += SPEED * delta;
-            isMoving = true;
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
-            position.x -= SPEED * delta;
-            isMoving = true;
-        }
-
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) { position.y += SPEED * delta; isMoving = true; lastDirection = Direction.UP; }
+        if (Gdx.input.isKeyPressed(Input.Keys.S)) { position.y -= SPEED * delta; isMoving = true; lastDirection = Direction.DOWN; }
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) { position.x += SPEED * delta; isMoving = true; lastDirection = Direction.RIGHT; }
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) { position.x -= SPEED * delta; isMoving = true; lastDirection = Direction.LEFT; }
 
         if (isMoving) {
             animTimer += delta;
-            if (animTimer >= frameDuration) {
+            if (animTimer >= FRAME_DURATION) {
                 animTimer = 0f;
-                showFrame1 = !showFrame1;
+                int frameCount = (lastDirection == Direction.LEFT || lastDirection == Direction.RIGHT) ? 2 : 3;
+                currentFrameIndex = (currentFrameIndex + 1) % frameCount;
             }
         } else {
-            showFrame1 = true;
+            currentFrameIndex = 0;
             animTimer = 0f;
         }
-        currentFrame = (isMoving && !showFrame1) ? left2 : left1;  // was just "currentFrame;"
-    }
 
+        switch (lastDirection) {
+            case LEFT:
+            case RIGHT:
+                currentFrame = (currentFrameIndex == 0) ? left1 : left2;
+                break;
+            case UP:
+                if (currentFrameIndex == 0)      currentFrame = up1;
+                else if (currentFrameIndex == 1) currentFrame = up2;
+                else                             currentFrame = up3;
+                break;
+            case DOWN:
+                if (currentFrameIndex == 0)      currentFrame = down1;
+                else if (currentFrameIndex == 1) currentFrame = down2;
+                else                             currentFrame = down3;
+                break;
+        }
+    }
     public void render(SpriteBatch batch) {
-        batch.draw(currentFrame, position.x - SIZE / 2f, position.y - SIZE / 2f, SIZE, SIZE);
+        float x = position.x - SIZE / 2f;
+        float y = position.y - SIZE / 2f;
+
+        if (lastDirection == Direction.RIGHT) {
+            batch.draw(currentFrame, x + SIZE, y, -SIZE, SIZE);
+        } else {
+            batch.draw(currentFrame, x, y, SIZE, SIZE);
+        }
     }
 
-    // Returns the player's center position, used by GameCamera to follow.
     public float getCenterX() { return position.x; }
     public float getCenterY() { return position.y; }
 
     public void dispose() {
-
-        left1.dispose();
-        left2.dispose();
+        left1.dispose(); left2.dispose();
+        up1.dispose();   up2.dispose();   up3.dispose();
+        down1.dispose(); down2.dispose(); down3.dispose();
     }
 }
