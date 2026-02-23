e07cac0c0baa2984dfff2c4958cd79481b972527
qdn <qdn@sfu.ca>
2026-02-22 16:25:39 -0800

    Make changes to GameLauncher.java

--- src/main/java/ca/sfu/spring2026team15/GameLauncher.java
+++ src/main/java/ca/sfu/spring2026team15/GameLauncher.java
@@ -2,16 +2,25 @@ package ca.sfu.spring2026team15;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import ca.sfu.spring2026team15.Main;
 
 public class GameLauncher {
     public static void main(String[] args) {
-        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-        config.setTitle("2D Game - CMPT 276");
-        config.setWindowedMode(1280, 900);
-        config.setForegroundFPS(60);
-        config.useVsync(true);
+        createApplication();
+    }
 
-        // Create and start the game
-        new Lwjgl3Application(new Main(), config);
+    private static Lwjgl3Application createApplication() {
+        return new Lwjgl3Application(new Main(), getDefaultConfiguration());
+    }
+
+    private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setTitle("Meowmino's Delivery");
+        config.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate);
+        config.setWindowedMode(1920, 1080);
+        config.setWindowIcon("catOnBike/catOnBike_Up.png");
+//        config.setForegroundFPS(60);
+        config.useVsync(true);
+        return config;
     }
 }
